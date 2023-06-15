6cee77aeb240dc3708c779c5934f07f4b4a6ee55
Gabriel Cavalcante <gg25gg26@hotmail.com>
2023-06-15 17:52:20 -0300

    criação do tabuleiro

--- src/main/java/br/ufrn/imd/modelo/Tabuleiro.java
+++ src/main/java/br/ufrn/imd/modelo/Tabuleiro.java
@@ -1,11 +1,27 @@
 package br.ufrn.imd.modelo;
 
-public class Tabuleiro {
+import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
+public class Tabuleiro extends Parent {
     //atributos
     private int qtd_navios;
-    private int linhas = 10;
+    private VBox linhas = new VBox();
     private int colunas = 10;
+
+    public Tabuleiro() {
+        this.qtd_navios = 5;
+        for(int y = 0; y< 10; y++){
+            HBox linha = new HBox();
+            for(int x = 0; x<colunas; x++){
+                Celula theCelula = new Celula(x, y, this);
+                linha.getChildren().add(theCelula);
+            }
+            linhas.getChildren().add(linha);
+        }
+        getChildren().add(linhas);
+    }
 
     public int getQtd_navios() {
         return qtd_navios;
