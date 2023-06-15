a18870a2879a885def5c9b5149a9d99656023540
Gabriel Cavalcante <gg25gg26@hotmail.com>
2023-06-15 18:06:06 -0300

    criação do tabuleiro

--- src/main/java/br/ufrn/imd/modelo/Celula.java
+++ src/main/java/br/ufrn/imd/modelo/Celula.java
@@ -1,13 +1,24 @@
 package br.ufrn.imd.modelo;
 
-public class Celula {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Celula extends Rectangle {
+
     private int x;
     private int y;
-    private boolean foiAtingio;
+    private boolean foiAtingido;
     private boolean temNavio;
     private Tabuleiro tabuleiro;
 
-
+    public Celula(int x, int y, Tabuleiro tabuleiro){
+        super(30,30);
+        this.x = x;
+        this.y = y;
+        this.tabuleiro = tabuleiro;
+        setFill(Color.LIGHTBLUE);
+        setStroke(Color.BLACK);
+    }
 
     public boolean foiAtingido(){
         return true;
