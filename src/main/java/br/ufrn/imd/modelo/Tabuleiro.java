71401dddefda0f94187d76e4f9fc556fa13214e9
fagnerdias <fagnerdiasmorais@gmail.com>
2023-06-15 12:03:44 -0300

    implementação base da classe tabuleiro

--- src/main/java/br/ufrn/imd/modelo/Tabuleiro.java
+++ src/main/java/br/ufrn/imd/modelo/Tabuleiro.java
@@ -2,4 +2,23 @@ package br.ufrn.imd.modelo;
 
 public class Tabuleiro {
 
+    //atributos
+    private int qtd_navios;
+    private int linhas = 10;
+    private int colunas = 10;
+
+    public int getQtd_navios() {
+        return qtd_navios;
+    }
+
+    public void setQtd_navios(int qtd_navios) {
+        this.qtd_navios = qtd_navios;
+    }
+
+    void posicionar_navio() {
+
+    }
+    void atirar() {
+
+    }
 }
